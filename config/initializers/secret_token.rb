840a4e92d087e5dfcfed2a5b69cf42925fb93f65
Nicolas Tonnelier <nt.nores@gmail.com>
2014-05-08 15:26:49 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wishlister::Application.config.secret_key_base = '6378f7cbf209e52c5b52bbf276f0eeb680c9000a837a930be47f7f2bff839808a7d71a9e49f39d1be26ed60ce89b68ab0c7632c3601426d7bdd94426618a70be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wishlister::Application.config.session_store :cookie_store, key: '_wishlister_session'
